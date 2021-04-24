7bc133fe3ceedf00c49fc3fe26ef2bb038f028cc
vivedo <edoardo.viviani@gmail.com>
2021-04-24 15:17:18 +0200

    Added production globals

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,7 @@
+const dev = process.env.NODE_ENV === 'development'
+
 const globals = {
-    API_URL: 'http://localhost:3200'
+    API_URL: dev ? 'http://localhost:3200' : 'http://api.vaccini.vivedo.me'
 }
 
 export default globals

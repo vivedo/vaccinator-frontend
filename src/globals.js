166ac20a6c9fcf4ca9749b826b93b8c1887b8d24
vivedo <edoardo.viviani@gmail.com>
2021-04-29 13:48:10 +0200

    removed protocol

--- src/globals.js
+++ src/globals.js
@@ -1,7 +1,7 @@
 const dev = process.env.NODE_ENV === 'development'
 
 const globals = {
-    API_URL: dev ? 'http://localhost:3200' : 'http://api.vaccini.vivedo.me'
+    API_URL: dev ? 'http://localhost:3200' : '//api.vaccini.vivedo.me'
 }
 
 export default globals
